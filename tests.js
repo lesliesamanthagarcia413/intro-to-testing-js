1308ad4a6576e60e5805229a19e4bc49af48f0a9
Leslie Samantha Garcia <leslie.samantha.garcia@gmail.com>
2020-06-23 15:23:48 -0500

    add the first test solution for the intro-to-testing

--- tests.js
+++ tests.js
@@ -13,3 +13,10 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe("sayHello", function () {
+    it("should be defined function", function () {
+    expect(typeof sayHello).toBe("function");
+    })
+
+})
